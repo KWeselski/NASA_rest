a6068711f735b4b46f79d4297d34727b51aa7240
KWeselski <weselski.kamil@gmail.com>
2020-11-29 12:07:49 +0100

    Add states in tempdiv

--- mars_rest/frontend/src/components/ActualTempDiv.js
+++ mars_rest/frontend/src/components/ActualTempDiv.js
@@ -5,8 +5,10 @@ import Toogle from './Toogle.js'
 export default class TemperatureDiv extends Component{      
     constructor(props){
         super (props);
-        this.state= {
+        this.state= {     
             toggled: true,
+            sol : null,
+            season: null,
             h_temp: null,
             m_temp: null,
             a_temp: null,
@@ -17,21 +19,25 @@ export default class TemperatureDiv extends Component{
             m_press: null,
             a_press: null,
             date: "",
+            direction : null
         };  
     };
 
     get_data(){
         let da = this.props.temp;
-        this.state.date = String(da.first_utc).split('T');
-        this.state.h_temp = String(da.max_temp).split('.')
-        this.state.m_temp = String(da.min_temp).split('.')
-        this.state.a_temp = String(da.avg_temp).split('.')
+        this.state.sol = da.Sol 
+        this.state.season = da.season;     
+        this.state.date = String(da.first_utc).split('T')[0];
+        this.state.h_temp = String(da.max_temp).split('.')[0];
+        this.state.m_temp = String(da.min_temp).split('.')[0];
+        this.state.a_temp = String(da.avg_temp).split('.')[0];
         this.state.h_wind = parseFloat(da.max_wind).toFixed(3)
         this.state.m_wind = parseFloat(da.min_wind).toFixed(3)
         this.state.a_wind = parseFloat(da.avg_wind).toFixed(3)
         this.state.h_press = parseFloat(da.max_press).toFixed(3)
         this.state.m_press = parseFloat(da.min_press).toFixed(3)
         this.state.a_press = parseFloat(da.avg_press).toFixed(3) 
+        this.state.direction = da.direction;
     }
 
     changeTempToF(temp){
@@ -47,36 +53,37 @@ export default class TemperatureDiv extends Component{
             this.state.h_temp = this.changeTempToF(this.state.h_temp);
             this.state.m_temp = this.changeTempToF(this.state.m_temp);
             this.state.a_temp= this.changeTempToF(this.state.a_temp);
+            this.state.h_temp = String(this.state.h_temp).split('.')[0];
+            this.state.m_temp = String(this.state.m_temp).split('.')[0];
+            this.state.a_temp = String(this.state.a_temp).split('.')[0];
         }
         else{
             this.state.h_temp = this.changeTempToC(this.state.h_temp);
             this.state.m_temp = this.changeTempToC(this.state.m_temp);
             this.state.a_temp= this.changeTempToC(this.state.a_temp);
+            this.state.h_temp = String(this.state.h_temp).split('.')[0];
+            this.state.m_temp = String(this.state.m_temp).split('.')[0];
+            this.state.a_temp = String(this.state.a_temp).split('.')[0];
 
         }  
     };
-   
 
     render(){
-        let da = this.props.temp;
         let celsius = "\u2103";
         let f = "\u2109";
-        let degree;
+        let degree;  
         if(this.state.h_temp == null){this.get_data()} 
-        if(this.state.toggled == true){celsius = this.switchTemperature(true); degree=f}
+        if(this.state.toggled == true){this.switchTemperature(true); degree=f}
         else{
-            this.switchTemperature(); degree=celsius;}
-        
-        
-        
+            this.switchTemperature(); degree=celsius;}    
         return(
             <Grid class="actualDiv" container>
                     <div>
-                        <h1>Sol: {da.Sol}</h1>
+                        <h1>Sol: {this.state.sol}</h1>
                         <h1>Day on Earth:</h1>
                         <h2>{this.state.date}</h2>
-                        <p>Season: {da.season}</p>
-                        <p>Wind Direction: {da.direction}</p> 
+                        <p>Season: {this.state.season}</p>
+                        <p>Wind Direction: {this.state.direction}</p> 
                                              
                     </div>
                     <div>
--- mars_rest/frontend/src/components/TempDiv.js
+++ mars_rest/frontend/src/components/TempDiv.js
@@ -3,33 +3,100 @@ import React, {Component } from 'react';
 
 export default class TemperatureDiv extends Component{
        
-    constructor(props,data){
+    constructor(props){
         super (props);
-    }
-    render(){
+        this.state= {
+            toggled: true,
+            sol : null,
+            h_temp: null,
+            m_temp: null,
+            a_temp: null,
+            h_wind: null,
+            m_wind: null,
+            a_wind: null,
+            h_press: null,
+            m_press: null,
+            a_press: null,
+            date: "",
+            direction : null,
+        }; 
+    };
+
+    get_data(){
         let da = this.props.temp;
-        let date = String(da.first_utc).split('T');
-        let h_temp = String(da.max_temp).split('.')
-        let m_temp = String(da.min_temp).split('.')
-        let a_temp = String(da.avg_temp).split('.')
-        let h_wind = parseFloat(da.max_wind).toFixed(3)
-        let m_wind = parseFloat(da.min_wind).toFixed(3)
-        let a_wind = parseFloat(da.avg_wind).toFixed(3)
+        this.state.sol = da.Sol;
+        this.state.direction = da.direction;
+        this.state.date = String(da.first_utc).split('T')[0];
+        this.state.h_temp = String(da.max_temp).split('.')[0]
+        this.state.m_temp = String(da.min_temp).split('.')[0]
+        this.state.a_temp = String(da.avg_temp).split('.')[0]
+        this.state.h_wind = parseFloat(da.max_wind).toFixed(3)
+        this.state.m_wind = parseFloat(da.min_wind).toFixed(3)
+        this.state.a_wind = parseFloat(da.avg_wind).toFixed(3)
+    }
+    /*
+    changeTempToF(temp){
+        return (temp*1.8) + 32;
+    };
+    changeTempToC(temp){
+        return (temp - 32)/ 1.8;
+    };
+    
+    componentDidMount(){
+        let degree; 
+        let celsius = "\u2103";
+        let f = "\u2109";
+        var toogle = document.getElementsByClassName('toggle')
+        console.log(toogle.toogled)
+        if (toogle.toogled == true){
+            this.state.toogled = true;
+            
+        }
+        else{ this.state.toogled = false;}       
+        if(this.state.toggled == true){this.switchTemperature(true); degree=f}
+        else{
+            this.switchTemperature(); degree=celsius;}
+        this.render()
+    }
+    
+    switchTemperature(tog){
+        let tog_ = tog;
+        if(tog_ == true){   
+            this.state.h_temp = this.changeTempToF(this.state.h_temp);
+            this.state.m_temp = this.changeTempToF(this.state.m_temp);
+            this.state.a_temp= this.changeTempToF(this.state.a_temp);
+            this.state.h_temp = String(this.state.h_temp).split('.')[0];
+            this.state.m_temp = String(this.state.m_temp).split('.')[0];
+            this.state.a_temp = String(this.state.a_temp).split('.')[0];
+        }
+        else{
+            this.state.h_temp = this.changeTempToC(this.state.h_temp);
+            this.state.m_temp = this.changeTempToC(this.state.m_temp);
+            this.state.a_temp= this.changeTempToC(this.state.a_temp);
+            this.state.h_temp = String(this.state.h_temp).split('.')[0];
+            this.state.m_temp = String(this.state.m_temp).split('.')[0];
+            this.state.a_temp = String(this.state.a_temp).split('.')[0];
+        }  
+    };*/
+
+
+    render(){
+        if(this.state.h_temp == null){this.get_data()}            
         return(
             <div class="tempDiv">
-                <h1>Sol: {da.Sol}</h1>
+                <h1>Sol: {this.state.sol}</h1>
+                
                 <div>
-                <p>High Temp:{h_temp[0]}&#8451;</p>
-                <p>Avg Temp:{a_temp[0]}&#8451;</p>
-                <p>Min Temp:{m_temp[0]}&#8451;</p>            
-                <p>Day: {date[0]}</p> 
+                <p>High Temp:{this.state.h_temp}</p>
+                <p>Avg Temp:{this.state.m_temp}</p>
+                <p>Min Temp:{this.state.a_temp}</p>            
+                <p>Day: {this.state.date}</p> 
                 </div>
                 <div>
-                    <p>Max speed:{h_wind}</p>
-                    <p>Avg speed:{m_wind}</p>
-                    <p>Min speed:{a_wind}</p>
-                    <p>Direction:{da.direction}</p>
-               
+                    <p>Max speed:{this.state.h_wind}</p>
+                    <p>Avg speed:{this.state.m_wind}</p>
+                    <p>Min speed:{this.state.a_wind}</p>
+                    <p>Direction:{this.state.direction}</p>    
                 </div>          
             </div>
 
